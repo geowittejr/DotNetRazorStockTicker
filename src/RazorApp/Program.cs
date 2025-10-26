226db654ecbfdf7b759ca74e54b7e38d67621664
George Witte <geowittejr@gmail.com>
2025-10-26 13:29:00 -0400

    Cleaned up the pages a little and removed the privacy page.

--- src/RazorApp/Program.cs
+++ src/RazorApp/Program.cs
@@ -21,35 +21,28 @@ builder.Services.AddScoped<IStockQuoteService, StockQuoteService>();
 builder.Services.AddScoped<ITickerService, TickerService>();
 builder.Services.AddScoped<ITickerCache, TickerCache>();
 
-
-/***************************
- * 
- * Exception Handling
- * https://learn.microsoft.com/en-us/aspnet/core/fundamentals/error-handling?view=aspnetcore-8.0
- * 
- */
-
 // Ensure we make IHttpClientFactory available for services that need it
 // https://learn.microsoft.com/en-us/aspnet/core/fundamentals/http-requests?view=aspnetcore-8.0
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    // Configure the HTTP request pipeline.
     app.UseExceptionHandler("/Error");
+
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
 
 app.Run();
