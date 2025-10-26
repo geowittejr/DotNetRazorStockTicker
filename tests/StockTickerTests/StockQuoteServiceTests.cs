5f24a211199f9d3887ab785ea9a3ac82caa934b0
George Witte <geowittejr@gmail.com>
2025-10-25 23:41:16 -0400

    Added a dummy test to start with.

---
+++ tests/StockTickerTests/StockQuoteServiceTests.cs
@@ -0,0 +1,42 @@
+using Castle.Core.Logging;
+using Microsoft.Extensions.Options;
+using StockTickerData.ConfigOptions;
+using StockTickerServices.Quotes;
+
+namespace StockTickerTests
+{
+    public class StockQuoteServiceTests
+    {
+        public IStockQuoteService GetStockQuoteService()
+        {
+            IOptions<StockQuotesOptions> options = Options.Create(new StockQuotesOptions
+            {
+                ApiKey = "test-api",
+                BaseUrl = "https://test-url.com"
+            });
+
+            return new StockQuoteService(
+                options,
+                null!,
+                null!);
+        }
+
+        [Fact]
+        public void StockQuoteService_DoSomething_ShouldNotThrowAnException()
+        {
+            var service = GetStockQuoteService();
+
+            try
+            {
+                service.DoSomething();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("DoSomething should not throw an exception.");
+            }
+
+            Assert.True(true);
+
+        }
+    }
+}
